2e1fabe9507d5b032d69c456d7d3cfc3ace531c9
northbrid <82058106+northbrid@users.noreply.github.com>
2026-08-14 23:54:41 +0000

    IoTLab: fixed Mac interface IP

--- IoTLab/netsim/1_macos_eth_conf.sh
+++ IoTLab/netsim/1_macos_eth_conf.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh -x
-sudo ifconfig en15 172.16.0.0 netmask 255.255.255.0 up
+
+# THING LEARNED: It was possible to set IP to 172.16.0.0
+# But I get "ping: sendto: Host is down" if I use that
+
+sudo ifconfig en15 172.16.0.10 netmask 255.255.255.0 up
 sudo cp dnsmasq.conf /opt/homebrew/etc/dnsmasq.conf
 sudo dnsmasq --no-daemon --log-dhcp -d
-
